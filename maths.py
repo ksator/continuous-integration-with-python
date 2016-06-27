103c009a80db466c70ad3268db1dcb041ad52fc0
ksator <khelil.sator@gmail.com>
2016-06-27 17:35:16 +0200

    Update maths.py

--- maths.py
+++ maths.py
@@ -2,12 +2,7 @@ def multiple(first,second):
     return first * second
 def add(x,y):
     return x + y
-def divide(a,b):
-    return a / b
 def subs(a,b):
     return a - b
-    
-
-
-
-
+def divide(a,b):
+    return a / b
