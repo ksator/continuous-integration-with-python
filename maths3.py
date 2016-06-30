c3dfd4c185649bd94ffe80d237b6a52130079c44
Khelil Sator <khelil.sator@gmail.com>
2016-07-01 01:03:04 +0200

    Create maths3.py

---
+++ maths3.py
@@ -0,0 +1,14 @@
+def multiple(first,second):
+    """Return multiplication of the 2 arguments.
+ 
+       Examples
+       --------
+       >>> multiple(4,5)
+       20
+       >>> multiple(2,1)
+       2
+    """
+    return first * second
+
+def add(x,y):
+    return x+y
